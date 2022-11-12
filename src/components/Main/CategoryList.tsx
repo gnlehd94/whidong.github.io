514137e8f718d9dbca1a757c3fda485c52b06d1c
[AI5/1반] 권휘동 <gnlehd94613@gmail.com>
2022-11-12 22:59:16 +0900

    카테고리 아이템 컴포넌트 구현하기

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,5 +1,16 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, ReactNode } from 'react'
 import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+type CategoryItemProps = {
+  active: boolean
+}
+
+type GatsbyLinkProps = {
+  children: ReactNode
+  className?: string
+  to: string
+} & CategoryItemProps
 
 export type CategoryListProps = {
   selectedCategory: string
@@ -15,6 +26,20 @@ const CategoryListWrapper = styled.div`
   width: 768px;
   margin: 100px auto 0;
 `
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
+  <Link {...props} />
+))<CategoryItemProps>`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  font-weight: ${({ active }) => (active ? '800' : '400')};
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
 
 const CategoryList: FunctionComponent<CategoryListProps> = function ({
   selectedCategory,
@@ -23,9 +48,13 @@ const CategoryList: FunctionComponent<CategoryListProps> = function ({
   return (
     <CategoryListWrapper>
       {Object.entries(categoryList).map(([name, count]) => (
-        <div key={name}>
+        <CategoryItem
+          to={`/?category=${name}`}
+          active={name === selectedCategory}
+          key={name}
+        >
           #{name}({count})
-        </div>
+        </CategoryItem>
       ))}
     </CategoryListWrapper>
   )
