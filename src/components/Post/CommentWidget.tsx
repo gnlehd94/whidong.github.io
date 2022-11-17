0f672b0d1107687a924306c1e98d88e924bcc87d
[AI5/1반] 권휘동 <gnlehd94613@gmail.com>
2022-11-17 12:23:42 +0900

    상단 부분 반응형 디자인

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -1,7 +1,7 @@
 import React, { createRef, FunctionComponent, useEffect } from 'react'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'gnlehd94/dongdong.github.io' // 자신 계정의 레포지토리로 설정
+const repo = 'gnlehd94/dongdong.github.io'
 
 type UtterancesAttributesType = {
   src: string
